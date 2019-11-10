57bb78192babdb10bfffb54c6ca1777425e374a9
Eric Leong <eleong95@gmail.com>
2019-11-10 07:51:47 -0800

    submenu submenu submenu

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -9,28 +9,60 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 import { Layout } from '~/src/components';
-import { Button, Menu, Icon } from 'antd';
+import { Button, Col, Icon, Menu, Row, Typography } from 'antd';
+const { SubMenu } = Menu;
 
-import { Row, Col, Typography } from 'antd';
 
 const { auth, firestore } = firebaseInit();
 
 const Dashboard = () => {
   const generateSidebarItems = () => {
     return (
-      <Menu defaultSelectedKeys={['1']}>
-        <Menu.Item key="1">
-          <Icon type="pie-chart" />
-          <span>Option 1</span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="desktop" />
-          <span>Option 2</span>
-        </Menu.Item>
-        <Menu.Item key="9">
-          <Icon type="file" />
-          <span>File</span>
-        </Menu.Item>
+      <Menu style={{ borderRight: 'none' }} selectable={false} mode={'inline'}>
+        <SubMenu
+          key="sub1"
+          title={
+            <span>
+              <Icon type="mail" />
+              <span>Navigation One</span>
+            </span>
+          }
+        >
+          <Menu.Item key="1">Option 1</Menu.Item>
+          <Menu.Item key="2">Option 2</Menu.Item>
+          <Menu.Item key="3">Option 3</Menu.Item>
+          <Menu.Item key="4">Option 4</Menu.Item>
+        </SubMenu>
+        <SubMenu
+          key="sub2"
+          title={
+            <span>
+              <Icon type="appstore" />
+              <span>Navigation Two</span>
+            </span>
+          }
+        >
+          <Menu.Item key="5">Option 5</Menu.Item>
+          <Menu.Item key="6">Option 6</Menu.Item>
+          <SubMenu key="sub3" title="Submenu">
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item>
+          </SubMenu>
+        </SubMenu>
+        <SubMenu
+          key="sub4"
+          title={
+            <span>
+              <Icon type="setting" />
+              <span>Navigation Three</span>
+            </span>
+          }
+        >
+          <Menu.Item key="9">Option 9</Menu.Item>
+          <Menu.Item key="10">Option 10</Menu.Item>
+          <Menu.Item key="11">Option 11</Menu.Item>
+          <Menu.Item key="12">Option 12</Menu.Item>
+        </SubMenu>
       </Menu>
 
     );
@@ -48,7 +80,6 @@ const Dashboard = () => {
           <a>THIS IS THE MOTHERFUCKING DASHBOARD YOU BITCH</a>
         </Link>
       </Row>
-
     </Layout>
   );
 };
