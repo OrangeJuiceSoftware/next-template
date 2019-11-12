8e36e4922990812670494576c4f7152ff100cf7a
Eric Leong <eleong95@gmail.com>
2019-11-12 13:01:22 -0800

    updated styles and colors

--- src/components/layouts/sidebar.js
+++ src/components/layouts/sidebar.js
@@ -4,8 +4,14 @@ import { Layout, Row, Col, Menu, Icon } from 'antd';
 const { Sider } = Layout;
 
 const Sidebar = ({ width, items, style }) => {
+  const _style = {
+    borderTopRightRadius: 30,
+    borderBottomRightRadius: 30,
+    ...style
+  };
+
   return (
-    <Sider theme={'light'} width={width} style={style}>
+    <Sider theme={'light'} width={width} style={ _style }>
       {items}
     </Sider>
   );
